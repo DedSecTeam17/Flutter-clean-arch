46c00850983fa6182ad15ea8d5887e11d5e59b01
Mohammed_Elamin <melamin100@yahoo.com>
2020-12-03 08:59:40 +0200

    start working on data layer by creating models and do tdd testing 4 them

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:tdd_tut/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(
+          text: text,
+          number: number,
+        );
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+
+
+}
